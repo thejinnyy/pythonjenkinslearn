8954b69a46affda59522668d99dab5f8c2c8aa23
thejinnyy <thejinnyy@gmail.com>
2026-01-31 18:13:36 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,20 +3,20 @@ import json
 import pytest
 
 
-def pytest_addoption(parser):
+def pytest_addoption(config):
     parser.addoption("--host", action="store", default="prod")
+    # Get the folder where conftest.py is located
+    base_dir = os.path.dirname(__file__)
+    
+    # Build path to endpoints.json relative to conftest.py
+    file_path = os.path.join(base_dir, "config", "endpoints.json")
+
 
 
 def update_env(config):
-    with open("/Users/payzapp-automation/PycharmProjects/api_test/config/endpoints.json", "r+") as jsonFile:
+    with open(file_path, "r+") as jsonFile:
         data = json.load(jsonFile)
         jsonFile.truncate(0)
         jsonFile.seek(0)
         data['environment']['env'] = config.getoption("--host").lower()
         json.dump(data, jsonFile, indent=4)
-        jsonFile.close()
-
-
-@pytest.hookimpl()
-def pytest_configure(config):
-    update_env(config)
